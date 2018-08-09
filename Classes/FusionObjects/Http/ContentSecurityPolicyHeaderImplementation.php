ca99b5de2a71704a11a93a11ba3f03c9a8d82982
Lars Nieuwenhuizen <l.nieuwenhuizen@developers.nl>
2018-08-09 14:08:48 +0200

    FEAT: Add different csp for backend

--- Classes/FusionObjects/Http/ContentSecurityPolicyHeaderImplementation.php
+++ Classes/FusionObjects/Http/ContentSecurityPolicyHeaderImplementation.php
@@ -5,7 +5,8 @@ namespace Nieuwenhuizen\ContentSecurityPolicy\FusionObjects\Http;
 
 use Neos\Fusion\FusionObjects\Http\ResponseHeadImplementation;
 use Neos\Flow\Annotations as Flow;
-use Nieuwenhuizen\ContentSecurityPolicy\ContentSecurity\ContentSecurityPolicyImplementation;
+use Nieuwenhuizen\ContentSecurityPolicy\Domain\Model\Policy;
+use Nieuwenhuizen\ContentSecurityPolicy\Exceptions\PolicyConstructionException;
 use Nieuwenhuizen\ContentSecurityPolicy\Factory\PolicyFactory;
 
 class ContentSecurityPolicyHeaderImplementation extends ResponseHeadImplementation
@@ -32,10 +33,39 @@ class ContentSecurityPolicyHeaderImplementation extends ResponseHeadImplementati
         $headers = parent::getHeaders();
 
         if ($this->enabled) {
-            $policy = PolicyFactory::create($this->configuration['default'], $this->configuration['custom']);
+            $policy = $this->backendOrFrontendPolicy();
             $headers[$policy->getSecurityHeaderKey()] = (string)$policy;
         }
 
         return $headers;
+    }
+
+    /**
+     * @return Policy
+     * @throws PolicyConstructionException
+     */
+    protected function backendOrFrontendPolicy(): Policy
+    {
+        if ($this->getContext()['inBackend'] === true) {
+            $policy = PolicyFactory::create(
+                $this->configuration['backend'],
+                $this->configuration['custom-backend']
+            );
+        } else {
+            $policy = PolicyFactory::create(
+                $this->configuration['default'],
+                $this->configuration['custom']
+            );
+        }
+
+        return $policy;
+    }
+
+    /**
+     * @return array
+     */
+    protected function getContext(): array
+    {
+        return $this->fusionValue('context');
     }
 }
